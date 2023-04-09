2c41a26f80a918146b02b58b203b42e5bc597bf3
Hana Rasheed <43776499+HanaZRasheed@users.noreply.github.com>
2023-04-09 10:43:46 +0300

    Insert At a position

---
+++ 5- linkedlist V2/MyLinkedList.java
@@ -0,0 +1,57 @@
+package ds.linkedlist;
+
+public class MyLinkedList {
+
+	private Node head ;
+	private class Node{
+		// 1- store the data 
+		int data;
+		// 2- 
+		Node next;
+		
+		Node(int data){
+			this.data=data;
+		}
+	}
+	
+	
+	public void insert(int data) {
+		Node newNode=new Node(data);
+		if(head==null) {
+		//	System.out.println(" NewNode address "+ newNode);
+			head=newNode;
+		// 	System.out.println(" head address "+ head);
+		}else {
+			Node iterator=head;
+			while(iterator.next!=null) {
+				iterator=iterator.next;
+			}
+			iterator.next=newNode;
+		//	System.out.println(iterator.next);
+		}
+	}
+	
+	public void insertAtPosition(int data,int pos) {
+		Node newNode=new Node(data);
+		if(pos==0) {
+			newNode.next=head;
+			head=newNode;
+		} else {
+			Node iterator=head;
+			for(int i=1; i<pos;i++) {
+				iterator=iterator.next;
+			}
+			newNode.next=iterator.next;
+			iterator.next=newNode;
+		}
+	}
+	
+	public void print() {
+		Node iterator=head;
+		while(iterator!=null) {
+			System.out.print(iterator.data+ " ");
+			iterator=iterator.next;
+		}
+		System.out.println();
+	}
+}
