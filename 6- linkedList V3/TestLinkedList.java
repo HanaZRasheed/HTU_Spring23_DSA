4c96a17f1358bafeebda18cc9e69e42a97f2b376
Hana Rasheed <43776499+HanaZRasheed@users.noreply.github.com>
2023-04-11 15:55:42 +0300

    Linked list with removeAtPosition

---
+++ 6- linkedList V3/TestLinkedList.java
@@ -0,0 +1,15 @@
+package ds.linkedlist;
+
+public class TestLinkedList {
+public static void main(String[] args) {
+	MyLinkedList linked=new MyLinkedList();
+	linked.insert(10);
+	linked.insert(20);
+	linked.print();
+	linked.insertAtPosition(30, 0);
+	linked.print();
+	linked.removeAt(1);
+	linked.print();
+	
+}
+}
